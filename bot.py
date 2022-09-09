57dbafe5c42467e0a1527ab8aad9df46cff70949
erix-xiii <111148159+erix-xiii@users.noreply.github.com>
2022-09-09 15:43:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 #(©)Codexbotz
+#rewriten by @lifeinsinn @xproject13
 
 import pyromod.listen
 from pyrogram import Client
@@ -35,26 +36,26 @@ class Bot(Client):
                     link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
                 self.invitelink = link
             except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                self.LOGGER('[Sinner-X]').warning(a)
+                self.LOGGER('[Sinner-X]').warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER('[Sinner-X]').warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER('[Sinner-X]').info("\nBot Stopped. Join https://t.me/xxxsquad18plus for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Memeq")
             await test.delete()
         except Exception as e:
-            self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+            self.LOGGER('[Sinner-X]').warning(e)
+            self.LOGGER('[Sinner-X]').warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER('[Sinner-X]').info("\nBot Stopped. Join https://t.me/xxxsquad18plus for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        self.LOGGER('[Sinner-X]').info(f"Bot Ngentod..!\n\nCreated by \nhttps://t.me/lifeinsinn")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+        self.LOGGER('[Sinner-X]').info("Bot stopped.")
