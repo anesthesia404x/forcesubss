88944ba457528c2cb737300342b44c7436670271
erix-xiii <111148159+erix-xiii@users.noreply.github.com>
2022-09-09 15:58:22 +0700

    Update cbb.py

--- zero/cbb.py
+++ zero/cbb.py
@@ -1,4 +1,5 @@
 #(©)Codexbotz
+#rewriten by @lifeinsinn
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/erix-xiii/ZeroFileSharing'>Click here</a>\n○ Channel : @xproject13\n○ Support Group : @xxxsquad18plus</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
