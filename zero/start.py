a1e6b9612a235f57a8a9c0bb73d7e1c42a6cc9ce
hydra-13 <105912211+hydra-13@users.noreply.github.com>
2022-10-24 13:50:00 +0700

    Update start.py

--- zero/start.py
+++ zero/start.py
@@ -101,7 +101,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await message.reply_video(START_VID, 
-            text = START_MSG.format(
+            START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
