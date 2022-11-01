46c9187b81ff156796a75c327cc0da656b70e187
hydra-13 <105912211+hydra-13@users.noreply.github.com>
2022-11-01 13:12:30 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@
 
 import pyromod.listen
 from pyrogram import Client
-from pyrogram.enums import ParseMode
+#from pyrogram.enums import ParseMode
 import sys
-from datetime import datetime
+#from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID
 
