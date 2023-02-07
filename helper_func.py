e29db777368163a4f60007c41e207c5cae095f44
anesthesia404x <115077016+anesthesia404x@users.noreply.github.com>
2023-02-07 17:11:00 +0800

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -4,7 +4,7 @@ import base64
 import re
 import asyncio
 from pyrogram import filters
-#from pyrogram.enums import ChatMemberStatus
+from pyrogram.enums import ChatMemberStatus
 from config import FORCE_SUB_CHANNEL, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
